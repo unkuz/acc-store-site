56bfd84a7165aece443fb9d2ec6a5ac22910cdc8
unkuz <hi.unkuz@gmail.com>
2024-05-19 17:05:08 +0700

    🌵

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,14 +1,17 @@
 <template>
   <header
-    class="fixed left-1/2 top-[20px] z-10 flex h-[60px] w-[90vw] -translate-x-1/2 items-center justify-between rounded-[15px] bg-[#74747475] px-[20px] backdrop-blur-[2px] xl:w-[1000px]"
+    class="fixed left-1/2 top-[20px] z-10 grid h-[60px] w-[90vw] -translate-x-1/2 grid-cols-3 rounded-[10px] bg-[#69cbf8] px-[20px] backdrop-blur-[2px] xl:w-[1000px]"
   >
-    <div>Logo</div>
-    <div class="flex h-[40px] w-[250px] items-center rounded-[10px] bg-yellow-200 px-[15px]">
-      <span>Search</span>
+    <div class="flex h-full w-full items-center justify-start">ShopTkGiaRe</div>
+    <div class="flex h-full w-full items-center justify-center">
+      <div class="flex h-[40px] w-[350px] items-center rounded-[10px] bg-[#ffffff] px-[15px]">
+        <span>Search</span>
+      </div>
     </div>
-    <nav class="flex gap-[20px]">
-      <span>Tracking</span>
-      <span>Gio hang</span><span>Login</span>
+
+    <nav class="flex gap-[20px] items-center justify-end">
+      <span>Kiểm tra order</span>
+      <span>Giỏ hàng</span><span>Đăng nhập</span>
     </nav>
   </header>
 </template>
