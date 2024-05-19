243b72e06919f3d2c3dc7c7b9ff3cb738547471f
unkuz <hi.unkuz@gmail.com>
2024-05-20 06:30:33 +0700

    🍇

--- components/Layout/Header/Header.vue
+++ components/Layout/Header/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <header
-    class="fixed w-full left-0 top-0 z-10 grid h-[60px] grid-cols-3 border-b-[2px] border-b-[#7979791a] bg-[#ffffffd5] px-[10px] backdrop-blur-[3px] dark:bg-black sm:h-[80px] lg:h-[100px] lg:px-[30px]"
+    class="fixed left-0 top-0 z-10 grid h-[60px] w-full grid-cols-4 border-b-[2px] border-b-[#7979791a] bg-[#ffffffd5] px-[10px] backdrop-blur-[3px] dark:bg-black sm:h-[80px] lg:h-[100px] lg:grid-cols-3 lg:px-[30px]"
   >
     <NuxtLink to="/" class="w-[60px] sm:w-[80px] lg:w-[100px]">
       <NuxtPicture format="avif,webp" src="/logo-min.png" />
     </NuxtLink>
 
-    <div class="flex h-full w-full items-center justify-center">
-      <Search class="lg:w-[700px] w-[130%]" />
+    <div class="col-span-2 flex h-full w-full items-center justify-center lg:col-span-1">
+      <Search class="lg:w-[700px]" />
     </div>
 
-    <nav class=" items-center justify-end gap-[30px] lg:flex hidden">
+    <nav class="hidden items-center justify-end gap-[30px] lg:flex">
       <UToggle
         on-icon="i-heroicons-moon"
         off-icon="i-heroicons-sun"
@@ -26,6 +26,15 @@
         title="Đăng nhập"
       />
     </nav>
+    <div class="flex h-full w-full items-center justify-end">
+      <div
+        class="flex h-[40px] w-[40px] flex-col items-center justify-center gap-[2px] rounded-[10px] border-[1px] border-[#7777776c] bg-transparent shadow-sm *:h-[5px] *:w-[5px] *:rounded-[50%] *:bg-[#414141] sm:h-[50px] sm:w-[50px] *:sm:h-[7px] sm:*:w-[7px] lg:hidden"
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   </header>
 </template>
 <script lang="ts" setup>
